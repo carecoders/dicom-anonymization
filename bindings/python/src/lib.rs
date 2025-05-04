4213b31c742bbe4f8051c10dd99bcfa13fc1708f
Sander Smits <25479+jsmits@users.noreply.github.com>
2025-05-04 23:00:56 +0200

    Raise Python exceptions if something goes wrong

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,16 +1,23 @@
-use std::fs::File;
-
 use dicom_anonymization::Anonymizer;
+use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
+use std::fs::File;
 
 /// Anonymize a DICOM file at the given path and return the anonymized DICOM file as a byte array.
 #[pyfunction]
 fn anonymize(file_path: &str) -> PyResult<Vec<u8>> {
     let anonymizer = Anonymizer::default();
+
     let file = File::open(file_path)?;
-    let result = anonymizer.anonymize(file).unwrap();
+    let result = anonymizer
+        .anonymize(file)
+        .map_err(|e| PyErr::new::<PyException, _>(e.to_string()))?;
+
     let mut output = Vec::<u8>::new();
-    result.write(&mut output).unwrap();
+    result
+        .write(&mut output)
+        .map_err(|e| PyErr::new::<PyException, _>(e.to_string()))?;
+
     Ok(output)
 }
 
