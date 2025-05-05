72ac81afc3c6a9408dc19876b46eb25763230825
Sander Smits <25479+jsmits@users.noreply.github.com>
2025-05-05 12:05:06 +0200

    Improve Python bindings interface

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,14 +1,40 @@
 use dicom_anonymization::Anonymizer;
 use pyo3::exceptions::PyException;
 use pyo3::prelude::*;
+use pyo3_file::PyFileLikeObject;
 use std::fs::File;
+use std::io::Read;
 
-/// Anonymize a DICOM file at the given path and return the anonymized DICOM file as a byte array.
+/// Represents either a `FilePath` or a `FileLike` object
+#[derive(Debug)]
+enum FilePathOrFileLike {
+    FilePath(String),
+    FileLike(PyFileLikeObject),
+}
+
+impl<'py> FromPyObject<'py> for FilePathOrFileLike {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        // file path
+        if let Ok(string) = ob.extract::<String>() {
+            return Ok(FilePathOrFileLike::FilePath(string));
+        }
+
+        // file-like
+        let f = PyFileLikeObject::py_with_requirements(ob.clone(), true, false, true, false)?;
+        Ok(FilePathOrFileLike::FileLike(f))
+    }
+}
+
+/// Anonymize a DICOM object and return the anonymized DICOM object as bytes.
 #[pyfunction]
-fn anonymize(file_path: &str) -> PyResult<Vec<u8>> {
+fn anonymize(fp: FilePathOrFileLike) -> PyResult<Vec<u8>> {
     let anonymizer = Anonymizer::default();
 
-    let file = File::open(file_path)?;
+    let file: Box<dyn Read> = match fp {
+        FilePathOrFileLike::FilePath(s) => Box::new(File::open(s)?),
+        FilePathOrFileLike::FileLike(f) => Box::new(f),
+    };
+
     let result = anonymizer
         .anonymize(file)
         .map_err(|e| PyErr::new::<PyException, _>(e.to_string()))?;
@@ -25,5 +51,6 @@ fn anonymize(file_path: &str) -> PyResult<Vec<u8>> {
 #[pymodule]
 fn dcmanon(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(anonymize, m)?)?;
+
     Ok(())
 }
