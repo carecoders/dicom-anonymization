1d37f274022b0f2167380161ae533083103f7ec6
Sander Smits <25479+jsmits@users.noreply.github.com>
2025-05-09 23:29:55 +0200

    Simplify Python bindings for Anonymizer class
    Replace verbose constructor with one that has default parameters, change
    uid_root to take &str instead of String, and remove redundant with_config
    method. Also clean up commented code.

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -27,7 +27,7 @@ impl<'py> FromPyObject<'py> for FilePathOrFileLike {
     }
 }
 
-/// Python class that wraps the Rust Anonymizer
+/// Anonymizer class
 #[pyclass]
 struct Anonymizer {
     inner: RustAnonymizer,
@@ -35,40 +35,20 @@ struct Anonymizer {
 
 #[pymethods]
 impl Anonymizer {
-    /// Create a new Anonymizer with optional configuration
+    /// Create a new Anonymizer instance
     #[new]
-    fn new(uid_root: Option<String>, remove_private_tags: Option<bool>) -> Self {
+    #[pyo3(signature = (uid_root="9999", remove_private_tags=false))]
+    fn new(uid_root: Option<&str>, remove_private_tags: Option<bool>) -> Self {
         let mut builder = ConfigBuilder::default();
 
         if let Some(uid_root) = uid_root {
-            // TODO: handle `Err`
+            // TODO: handle `Err` instead of unwrap
             let uid_root = UidRoot::new(&uid_root).unwrap();
             builder = builder.uid_root(uid_root);
         }
 
         if let Some(remove_private) = remove_private_tags {
-            if remove_private {
-                builder = builder.remove_private_tags(true);
-            }
-        }
-
-        let config = builder.build();
-        let processor = DefaultProcessor::new(config);
-
-        let anonymizer = RustAnonymizer::new(processor);
-
-        Anonymizer { inner: anonymizer }
-    }
-
-    /// Create a new Anonymizer with custom configuration
-    #[staticmethod]
-    fn with_config(remove_private_tags: Option<bool>) -> Self {
-        let mut builder = ConfigBuilder::default();
-
-        if let Some(remove_private_tags) = remove_private_tags {
-            if remove_private_tags {
-                builder = builder.remove_private_tags(true);
-            }
+            builder = builder.remove_private_tags(remove_private);
         }
 
         let config = builder.build();
@@ -100,20 +80,10 @@ impl Anonymizer {
     }
 }
 
-// /// Anonymize a DICOM object and return the anonymized DICOM object as bytes.
-// ///
-// /// This is a convenience function that uses the default Anonymizer.
-// #[pyfunction]
-// fn anonymize(fp: FilePathOrFileLike) -> PyResult<Vec<u8>> {
-//     let anonymizer = Anonymizer::new();
-//     anonymizer.anonymize(fp)
-// }
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn dcmanon(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<Anonymizer>()?;
-    // m.add_function(wrap_pyfunction!(anonymize, m)?)?;
 
     Ok(())
 }
