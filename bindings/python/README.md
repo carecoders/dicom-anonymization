6268ba9de549d3f61733cbc058abdd8f82c0415b
Sander Smits <25479+jsmits@users.noreply.github.com>
2025-05-21 21:46:57 +0200

    Python improvements

--- bindings/python/README.md
+++ bindings/python/README.md
@@ -1,3 +1,39 @@
-# Bindings for Python
+# dcmanon
 
-Not released yet. Work in progress.
+Lightning-fast DICOM anonymization for Python, built in Rust.
+
+## Installation
+
+```bash
+pip install dcmanon
+```
+
+## Usage
+
+```Python
+from dcmanon import Anonymizer
+
+uid_root = "9999.12.3.4.5"
+
+# override some default tag actions
+tag_actions = {
+    "00080050": {
+        "comment": "AccessionNumber",
+        "action": "hash",
+        "length": 10,
+    },
+    "00100010": {
+        "comment": "PatientName",
+        "action": "replace",
+        "value": "John Doe",
+    }
+}
+
+file_path = "/path/to/dicom.dcm"  # change this
+
+anonymizer = Anonymizer(uid_root=uid_root, tag_actions=tag_actions)
+anonymized_dicom_as_bytes = anonymizer.anonymize(file_path)
+
+output_file = open("anonymized.dcm", "wb")
+output_file.write(anonymized_dicom_as_bytes)
+```
--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -1,7 +1,9 @@
+use dicom_anonymization::actions::hash::HASH_LENGTH_MINIMUM;
+use dicom_anonymization::actions::Action;
 use dicom_anonymization::config::builder::ConfigBuilder;
 use dicom_anonymization::config::uid_root::UidRoot;
 use dicom_anonymization::processor::DefaultProcessor;
-use dicom_anonymization::Anonymizer as RustAnonymizer;
+use dicom_anonymization::{Anonymizer as RustAnonymizer, Tag};
 use pyo3::create_exception;
 use pyo3::exceptions::{PyException, PyIOError, PyValueError};
 use pyo3::prelude::*;
@@ -48,33 +50,97 @@ struct Anonymizer {
 impl Anonymizer {
     /// Create a new Anonymizer instance
     #[new]
-    #[pyo3(signature = (uid_root=None, config=None))] // TODO: change config to tag_actions
-    fn new(uid_root: Option<&str>, config: Option<&Bound<'_, PyDict>>) -> PyResult<Self> {
+    #[pyo3(signature = (uid_root=None, tag_actions=None))]
+    fn new(uid_root: Option<&str>, tag_actions: Option<&Bound<'_, PyDict>>) -> PyResult<Self> {
         let mut builder = ConfigBuilder::default();
 
-        // Apply individual parameters (they take precedence)
+        // Apply uid root if provided
         if let Some(uid_root) = uid_root {
             let uid_root =
                 UidRoot::new(uid_root).map_err(|e| PyErr::new::<PyValueError, _>(e.to_string()))?;
             builder = builder.uid_root(uid_root);
         }
 
-        // Apply settings from the config dictionary
-        if let Some(config_dict) = config {
-            // Process uid_root if not already set via parameter
-            if uid_root.is_none() && config_dict.contains("uid_root")? {
-                let uid_root_str: String = config_dict.get_item("uid_root")?.unwrap().extract()?;
-                let uid_root = UidRoot::new(&uid_root_str)
-                    .map_err(|e| PyErr::new::<PyValueError, _>(e.to_string()))?;
-                builder = builder.uid_root(uid_root);
-            }
+        // Apply tag actions if provided
+        if let Some(tag_actions_dict) = tag_actions {
+            for item in tag_actions_dict.iter() {
+                let (tag, action) = item;
 
-            // Additional config options can be added here following the same pattern
-            // Example:
-            // if config_dict.contains("option_name")? {
-            //     let value: ValueType = config_dict.get_item("option_name")?.extract()?;
-            //     builder = builder.option_name(value);
-            // }
+                // get the tag
+                let tag_str: String = tag.extract()?;
+                let tag: Tag = tag_str.parse().map_err(|_| {
+                    PyErr::new::<PyValueError, _>(format!("Failed to parse tag {}", tag_str))
+                })?;
+
+                // get the action
+                let action_dict: Bound<'_, PyDict> = action.extract()?;
+                let action_str = action_dict.get_item("action")?;
+                let action = if let Some(action_str) = action_str {
+                    let action_str: &str = action_str.extract()?;
+                    match action_str {
+                        "empty" => Action::Empty,
+                        "hashdate" => Action::HashDate,
+                        "hashuid" => Action::HashUID,
+                        "keep" => Action::Keep,
+                        "none" => Action::None,
+                        "remove" => Action::Remove,
+                        "hash" => {
+                            let mut hash_length: Option<usize> = None;
+
+                            if let Some(length) = action_dict.get_item("length")? {
+                                hash_length = length.extract().map_err(|_| {
+                                    PyErr::new::<PyValueError, _>(format!(
+                                        "Failed to parse hash length for tag {}",
+                                        tag_str
+                                    ))
+                                })?;
+                            };
+
+                            if let Some(hash_length) = hash_length {
+                                if hash_length < HASH_LENGTH_MINIMUM {
+                                    return Err(PyErr::new::<PyValueError, _>(format!(
+                                        "Hash length must be at least {} (tag {})",
+                                        HASH_LENGTH_MINIMUM, tag_str
+                                    )));
+                                }
+                            }
+
+                            Action::Hash {
+                                length: hash_length,
+                            }
+                        }
+                        "replace" => {
+                            let replace_value =
+                                if let Some(value) = action_dict.get_item("value")? {
+                                    value.extract::<String>()?
+                                } else {
+                                    return Err(PyErr::new::<PyValueError, _>(format!(
+                                        "Failed to find replace value for tag {}",
+                                        tag_str
+                                    )));
+                                };
+
+                            Action::Replace {
+                                value: replace_value.into(),
+                            }
+                        }
+                        _ => {
+                            return Err(PyErr::new::<PyValueError, _>(format!(
+                                "Unsupported action '{}' for tag {}. Should be one of: hash, hashdate, hashuid, empty, remove, replace, keep, none.",
+                                action_str, tag_str
+                            )));
+                        }
+                    }
+                } else {
+                    return Err(PyErr::new::<PyValueError, _>(format!(
+                        "Failed to find action key for tag {}",
+                        tag_str
+                    )));
+                };
+
+                // Apply tag action to builder
+                builder = builder.tag_action(tag, action);
+            }
         }
 
         let config = builder.build();
